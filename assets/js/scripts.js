15b45f53f05ee4f0d2534769249a6159a7cc185d
admin <admin@admins-MacBook-Pro.local>
2022-05-31 15:26:42 -0400

    added js for same carousel div size

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,38 @@
+const windowEventListeners = [
+	'DOMContentLoaded',
+	'orientationchange',
+	'onresize',
+];
+
 const equalHeightDivs = (larger, smaller) => {
 	smaller.style.height = `${larger.offsetHeight}px`;
 };
 
-window.addEventListener('DOMContentLoaded', (event) => {
-	equalHeightDivs(
-		document.getElementById('video-title-larger'),
-		document.getElementById('video-title-smaller')
+const equalHeightDivsCarousel = (largest, allItems) => {
+	allItems.forEach(
+		(item) => (item.style.height = `${largest.offsetHeight}px`)
 	);
-});
+};
+
+windowEventListeners.forEach((item) =>
+	window.addEventListener(item, (event) => {
+		if (document.getElementById('video-title-larger')) {
+			equalHeightDivs(
+				document.getElementById('video-title-larger'),
+				document.getElementById('video-title-smaller')
+			);
+		}
+		if (document.getElementById('carousel-partings-largest')) {
+			equalHeightDivsCarousel(
+				document.getElementById('carousel-partings-largest'),
+				document.querySelectorAll('.carousel-item-partings')
+			);
+		}
+		if (document.getElementById('carousel-affiliations-largest')) {
+			equalHeightDivsCarousel(
+				document.getElementById('carousel-affiliations-largest'),
+				document.querySelectorAll('.carousel-item-affiliations')
+			);
+		}
+	})
+);
